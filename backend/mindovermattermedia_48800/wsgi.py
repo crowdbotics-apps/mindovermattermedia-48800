8561e85b67276c92ed1efca0608869a00ad24ced
crowdbotics-apps <team@crowdbotics.com>
2024-07-19 02:42:07 +0000

    Update models

--- backend/mindovermattermedia_48800/wsgi.py
+++ backend/mindovermattermedia_48800/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "mindovermattermedia_48800.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mindovermattermedia_48800.settings")
 
 application = get_wsgi_application()
